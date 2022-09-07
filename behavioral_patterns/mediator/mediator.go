b73f29a4a17a2792d0ed33be4ef3b195f8496592
erfidev <erfanhanifezade@gmail.com>
2022-09-07 23:11:24 +0430

    mediator

---
+++ behavioral_patterns/mediator/mediator.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+Mediator is a behavioral design pattern that reduces
+coupling between components of a program by making
+them communicate indirectly, through a special mediator
+object.
+*/
+
+func main() {
+
+}
